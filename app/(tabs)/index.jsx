72c2eb5f7fccdd1512a7697cb10f09cf087030c8
Sanju20020307 <157909917+Sanju20020307@users.noreply.github.com>
2025-07-04 13:12:27 +0530

    Refactor CustomButton components for improved readability and consistency

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -15,8 +15,6 @@ export default function Tab() {
         title="Signin"
         type="primary"
         onPress={() => router.push('/pages/SignInScreen.jsx')}
-        // style={{ backgroundColor: '#00e194' }}  
-        // textStyle={{ fontSize: 18, color: '#fff' }}
       />
 
       <CustomButton
@@ -24,46 +22,46 @@ export default function Tab() {
         type="primary"
         textStyle={{ color: colors.black }}
         onPress={() => router.push('/pages/SignInScreen.jsx')}
-        />
-<CustomButton
-        title= "Ratings"
+      />
+
+      <CustomButton
+        title="Ratings"
         type="primary"
         textStyle={{ color: colors.black }}
-        onPress={() => router.push('/pages/Ratings.jsx')}
-        />
+        onPress={() => router.push('/pages/Ratings')}
+      />
+
       <CustomButton
         title="Filters"
         type="primary"
         textStyle={{ color: colors.black }}
         onPress={() => router.push('/pages/Filters')}
-        />
+      />
       <CustomButton
         title="Planning a Route"
         type="primary"
         textStyle={{ color: colors.black }}
         onPress={() => router.push('/pages/TripPlanner1')}
-        />
+      />
       <CustomButton
         title="Route History"
         type="primary"
         textStyle={{ color: colors.black }}
         onPress={() => router.push('/pages/SavedTripsScreen')}
-        />
+      />
       <CustomButton
         title="Battery Status"
         type="primary"
         textStyle={{ color: colors.black }}
         onPress={() => router.push('/pages/BatteryStatusModal')}
-        />
+      />
 
       <CustomButton
         title="QuickCheck"
         type="primary"
         textStyle={{ color: colors.black }}
         onPress={() => router.push('/pages/QuickCheckModal')}
-        />
-
-
+      />
     </View>
   );
 }
