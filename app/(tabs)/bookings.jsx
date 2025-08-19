ccbaa97602270980a0f463412bfb44fe28d1d3f9
Mavishan01 <janithmavishan01@gmail.com>
2025-08-20 00:02:59 +0530

    upcoming bookings for the current user

--- app/(tabs)/bookings.jsx
+++ app/(tabs)/bookings.jsx
@@ -12,8 +12,10 @@ import colors from '../../constants/color';
 import fonts from '../../constants/fonts';
 import { API_BASE_URL } from '@env';
 import { useRouter , useLocalSearchParams} from 'expo-router';
+import useUserData from '../../hooks/useUserData';
 
 const BookingsScreen = () => {
+  const { user } = useUserData();
   const [activeTab, setActiveTab] = useState('Upcoming');
   const router = useRouter();
     const params = useLocalSearchParams();
@@ -36,7 +38,7 @@ const BookingsScreen = () => {
         Cancelled: 'getUserCancelledBookings',
       };
       const endpoint = endpointMap[activeTab];
-      const url = `${API_BASE_URL}/api/bookings/${endpoint}?ev_user_id=6849cbc0f3c3b1455d5c128b`;
+      const url = `${API_BASE_URL}/api/bookings/${endpoint}?ev_user_id=${user._id}`;
       console.log('Fetching from:', url);
 
       const response = await fetch(url, {
@@ -78,6 +80,7 @@ const BookingsScreen = () => {
   };
 
     fetchBookings();
+    // console.log('user: ', user);
   }, [activeTab]);
 
 
